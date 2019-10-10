79c145224103214e472245ee62ab8a8af612ba2e
Ana_stasya_S <anastasiya-s@live.de>
2019-10-10 20:25:50 +0300

    solution without redo disabling

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,52 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.state = config.initial;
+        this.states = config.states;
+        this.progress = [config.initial];
+        this.canceledStates = [];
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.state;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+        if (Object.keys(this.states).includes(state)) {
+            this.state = state;
+            this.progress.push(state);
+        } else throw Error("") 
+    }
 
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+        let currentState = this.states[this.state]
+        if(Object.keys(currentState.transitions).includes(event)) {
+            this.state = currentState.transitions[event];
+            this.progress.push(currentState.transitions[event]);
+        } else throw Error("")
+    }
+
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.progress.splice(1, this.progress.length - 1);
+        return this.state = this.progress[0];
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +56,51 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        if (event) {
+            return Object.entries(this.states)
+                        .filter(item=> Object.keys(item[1].transitions).includes(event))
+                        .map(item => item[0])
+        } else return Object.keys(this.states);
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
-
+    undo() {
+        if(this.progress.length > 1) {
+            const last = this.progress.pop()
+           this.canceledStates.push(last)
+           this.state = this.progress[this.progress.length - 1];
+           return true; 
+        } else return false
+    }
+    
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if(this.canceledStates.length > 0) {
+            let next = this.canceledStates.pop();
+           
+            this.state = next;
+            this.progress.push(next)
+            return true;
+        } else return false
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.progress = [];
+        this.currentStep = 0;
+
+    }
 }
 
 module.exports = FSM;
